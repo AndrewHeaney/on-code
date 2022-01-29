a09456c9cd0505dd7cf2cc05def5be142be730a6
Andrew Heaney <andrewheaney00@gmail.com>
2022-01-29 17:02:01 +0000

    Add jkx

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,29 +5,41 @@ import ProjectTile from "../components/project-tile"
 import ArtistDive from "../images/artist-dive.png"
 import UC from "../images/uc.png"
 import Gito from "../images/gito.png"
+import Jkx from "../images/jkx.png"
+
+import "../components/project-tile.css"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Projects" />
     <h1 style={{ textAlign: "center", marginBottom: "60px" }}>Projects</h1>
-    <div style={{ display: "inline-flex" }}>
-      <ProjectTile
-        title="Unidays Companion"
-        image={UC}
-        href={
-          "https://chrome.google.com/webstore/detail/unidays-companion/aagdmokijggnnehohbkjkphpllmpmlgm?hl=en-GB"
-        }
-      />
-      <ProjectTile
-        title="Artist Dive"
-        image={ArtistDive}
-        href={"https://artistdive.glitch.me/"}
-      />
-      <ProjectTile
-        title="GITO"
-        image={Gito}
-        href={"https://github.com/AndrewHeaney/gito"}
-      />
+    <div id="row">
+      <div id="col">
+        <ProjectTile
+          title="Unidays Companion"
+          image={UC}
+          href={
+            "https://chrome.google.com/webstore/detail/unidays-companion/aagdmokijggnnehohbkjkphpllmpmlgm?hl=en-GB"
+          }
+        />
+        <ProjectTile
+          title="GITO"
+          image={Gito}
+          href={"https://github.com/AndrewHeaney/gito"}
+        />
+      </div>
+      <div id="col">
+        <ProjectTile
+          title="Artist Dive"
+          image={ArtistDive}
+          href={"https://artistdive.glitch.me/"}
+        />
+        <ProjectTile
+          title="JKX"
+          image={Jkx}
+          href={"https://github.com/AndrewHeaney/json-key-explorer"}
+        />
+      </div>
     </div>
   </Layout>
 )
